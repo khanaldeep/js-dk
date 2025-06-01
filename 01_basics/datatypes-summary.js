1c47efea720bf71837420c12d5b2346e7a8c9e84
khanaldeep <135026961+khanaldeep@users.noreply.github.com>
2025-06-01 14:16:28 +0000

    dataTypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,6 +14,12 @@ const anotherId = Symbol('123')
 
 console.log(id == anotherId);
 
+
+const num = Number(456)
+const anotherNum = Number(456)
+
+console.log( num == anotherNum);
+
 // const bigNumber = 636123612939222222n
 
 
@@ -34,4 +40,34 @@ const myFunction = function() {
     
 }
 
-console.log(typeof id)
+console.log(typeof anotherId)
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (used in Primitive) , Heap (used in Non-Primitive)
+
+let myName = "DeepKhanal"
+
+let anothername = myName
+anothername = "KhanalDeep"
+
+console.log(myName);
+console.log(anothername);
+
+
+
+let userOne = {
+    email: "deep@gmail.com",
+    age: 22
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "flllas@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
