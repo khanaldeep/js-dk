8f5bd6f3aaf4f3071e2b2ab47dc61ea8960f638f
khanaldeep <135026961+khanaldeep@users.noreply.github.com>
2025-06-05 12:52:16 +0000

    Date

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,33 @@
+// Dates
+
+let myDates = new Date()
+// console.log(myDates.toString());
+// console.log(myDates.toISOString());
+// console.log(myDates.toLocaleString());
+// console.log(myDates.toDateString());
+// console.log(typeof myDates);
+
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+// let myCreateDate = new Date("2023-01-14")
+let myCreateDate = new Date("01-14-2025")
+
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+
+
+
+
